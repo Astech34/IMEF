1040b2cf6c3df4fdc981bbda10b4567638a3aa56
Brianna Isola <briannaisola@gmail.com>
2024-05-20 13:38:27 -0400

    WIP to hold plot functions

---
+++ imef-statistics/proposal-model/prop_visualizations.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+import xarray as xr
+from matplotlib import pyplot as plt, dates as mdates, ticker
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from scipy.stats import binned_statistic
+
+
+
